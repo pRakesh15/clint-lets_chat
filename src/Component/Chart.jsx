fa7f7f8500d892b0c99cc78f5dd879ba0b13e908
yiconic <rp5865442@gmail.com>
2024-01-26 02:03:21 +0530

    change color

--- src/Component/Chart.jsx
+++ src/Component/Chart.jsx
@@ -19,39 +19,29 @@ function Chart() {
   const [myid, setMyid] = useState("");
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState(null);
-  const[image,setImage]=useState(null);
-  const[rimage,setRimage]=useState(null);
- 
+  const [image, setImage] = useState(null);
+  const [rimage, setRimage] = useState(null);
+
   const sendM = (e) => {
-   
-      
-  
-    
-      const message = document.getElementById("chart").value;
-    socket.emit("message", {type:"message", message, myid });
+    const message = document.getElementById("chart").value;
+    socket.emit("message", { type: "message", message, myid });
 
     document.getElementById("chart").value = null;
+  };
+  const sendFile = (e) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const dataUrl = event.target.result;
+      socket.emit("sendFile", { type: "file", data: dataUrl });
+    };
 
-};
-const sendFile=(e)=>
-{
+    setImage();
+  };
 
-     const reader=new FileReader();
-     reader.onload=(event)=>
-     {
-      const dataUrl=event.target.result;
-      socket.emit("sendFile",{type:"file",data:dataUrl});
-     }
-    
-      setImage();
-      
-}
-
-const send=(event)=>
-{
-sendM();
-sendFile();
-}
+  const send = (event) => {
+    sendM();
+    sendFile();
+  };
 
   useEffect(() => {
     socket = io(ENDPOINT, { transports: ["websocket"] });
@@ -86,17 +76,17 @@ sendFile();
     socket.on("sendMessage", (data) => {
       setMessages([...messages, data]);
     });
- 
+
     return () => {
       socket.off();
     };
   }, [messages]);
   useEffect(() => {
     socket.on("reciveFile", (data) => {
-      setRimage([...rimage,data.data])
-      console.log(rimage)
+      setRimage([...rimage, data.data]);
+      console.log(rimage);
     });
- 
+
     return () => {
       socket.off();
     };
@@ -107,21 +97,21 @@ sendFile();
     // setText(image)
   };
 
-  const filechange=(e)=>
-
-  {
+  const filechange = (e) => {
     setImage(e.target.files.item(0));
-  
-    setText(image)
-  
-    
-  }
+
+    setText(image);
+  };
 
   return (
-    <div className=" h-screen w-fit    sm:w-fit  sm:h-screen lg:flex lg:items-center xl:flex xl:items-center bg-cyan-100">
+    <div className=" h-screen w-fit    sm:w-fit  sm:h-screen lg:flex lg:items-center xl:flex xl:items-center bg-cyan-100 ">
       <div className="  h-screen w-[100%] m-0 pb-0 lg:h-screen xl:h-screen">
-        <div className="flex bg-gray-700 h-12 box-border  items-center lg:justify-center xl:justify-center">
-          <img src={logo} alt="logo" className="w-28  mr-24 -ml-6 lg:w-24 lg:-mt-5 lg:-mb-5 lg:ml-1  xl:w-24  xl:-mt-5  xl:-mb-5  xl:ml-1" />
+        <div className="flex bg-gray-700 shadow-lg shadow-slate-900 box-border  items-center lg:justify-center xl:justify-center">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-28  mr-24 -ml-6 lg:w-24 lg:-mt-5 lg:-mb-5 lg:ml-1  xl:w-24  xl:-mt-5  xl:-mb-5  xl:ml-1"
+          />
           <div className="ml-28 text-2xl -mr-11 lg:ml-[1300px] xl:ml-[1300px] xl:mr-6 lg:mr-6">
             <MdDarkMode className="cursor-pointer text-2xl w-16  " />
           </div>
@@ -141,7 +131,7 @@ sendFile();
           })}
         </ScrollToBottom>
 
-        <div className="input flex bg-gray-900">
+        <div className="input flex bg-gray-600">
           <label htmlFor="upLodeFile">
             {" "}
             <GrAttachment className="w-20 mt-[18px]  -ml-4 text-2xl  cursor-pointer" />
@@ -159,12 +149,18 @@ sendFile();
             onChange={textchange}
             id="chart"
             placeholder="Type a message"
-            className="bg-gray-900 h-12 w-[90%] text-white text-xl outline-none"/>
-        
+            className="bg-gray-600 h-12 w-[90%] text-white text-xl outline-none"
+          />
+
           <button
             onClick={(e) => (!text ? e.preventDefault() : send())}
-            className="bg-transparent outline-none h-14 w-[5%] flex  items-center ml-10 lg:ml-14 xl:ml-14 ">
-            {!text ? "" : <IoSendSharp className="w-24 xl:w-28 lg:w-28 text-3xl text-white " />}
+            className="bg-transparent outline-none h-14 w-[5%] flex  items-center ml-10 lg:ml-14 xl:ml-14 "
+          >
+            {!text ? (
+              ""
+            ) : (
+              <IoSendSharp className="w-24 xl:w-28 lg:w-28 text-3xl text-white " />
+            )}
           </button>
         </div>
       </div>
